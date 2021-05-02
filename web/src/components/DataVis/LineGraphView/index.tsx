323cb458b6adb2279e7898a7d7d1359dc80d135e
RFJBraunstingl <rfj.braunstingl@gmail.com>
2021-05-02 11:07:08 +0200

    wire up line graph

--- web/src/components/DataVis/LineGraphView/index.tsx
+++ web/src/components/DataVis/LineGraphView/index.tsx
@@ -1,28 +1,41 @@
 import React from "react";
-import { Datapoint } from "../../../types/Datapoint";
-import { PlotTemplate } from "../../Plot/templates/PlotTemplate";
-import { LinePlot } from "../../Plot/templates/line/LinePlot";
-import { RandomDateData } from "../../../utils/DataGenerator";
-import { BasicAxis } from "../../Plot/templates/axis/BasicAxis";
-import { DateAxis } from "../../Plot/templates/axis/DateAxis";
-import { AxisAnchor } from "../../Plot/templates/axis/AxisConsts";
+import {Datapoint as DatastiDatapoint} from "../../../types/Datapoint";
+import {PlotTemplate} from "../../Plot/templates/PlotTemplate";
+import {LinePlot} from "../../Plot/templates/line/LinePlot";
+import {RandomDateData} from "../../../utils/DataGenerator";
+import {BasicAxis} from "../../Plot/templates/axis/BasicAxis";
+import {DateAxis} from "../../Plot/templates/axis/DateAxis";
+import {AxisAnchor} from "../../Plot/templates/axis/AxisConsts";
+import DataPoint, {DataType} from "../../../rest/dtos/DataPoint"
 
 interface Props {
-  data: Datapoint[];
+    data: DatastiDatapoint[];
 }
 
-const LineGraphView = ({ data }: Props) => {
-  const plotHeight = 600;
-  const plotWidth = 1000;
+const LineGraphView = ({data}: Props) => {
+    const plotHeight = 600;
+    const plotWidth = 1000;
 
-  return (
-    <PlotTemplate width={plotWidth} height={plotHeight}>
-      <LinePlot data={RandomDateData(31)}>
-        <BasicAxis anchor={AxisAnchor.LEFT} unit={"kg"} ticks={5} />
-        <DateAxis anchor={AxisAnchor.BOTTOM} unit={"kg"} ticks={30} />
-      </LinePlot>
-    </PlotTemplate>
-  );
+    const toDate: (timestamp: string) => Date = (timstamp) => {
+        return new Date(timstamp);
+    }
+
+    const convert: (data: DatastiDatapoint[]) => DataPoint[] = (data: DatastiDatapoint[]) => data
+        .filter(data => data.value !== undefined && data.timestamp !== undefined)
+        .map(value => new DataPoint(
+            DataType.NumberOverDate,
+            toDate(value.timestamp!!),
+            +value.value)
+        );
+
+    return (
+        <PlotTemplate width={plotWidth} height={plotHeight}>
+            <LinePlot data={convert(data)}>
+                <BasicAxis anchor={AxisAnchor.LEFT} unit={""} ticks={5}/>
+                <DateAxis anchor={AxisAnchor.BOTTOM} unit={"time"} ticks={5}/>
+            </LinePlot>
+        </PlotTemplate>
+    );
 };
 
 export default LineGraphView;
